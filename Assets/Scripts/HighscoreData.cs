af8c03dc921b928608aa20f2b1b052d17eb6599b
Utz Stauder <utzstauder@users.noreply.github.com>
2018-08-15 11:39:55 +0200

    Initial commit

---
+++ Assets/Scripts/HighscoreData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class HighscoreData {
+
+    [System.Serializable]
+    public class HighscoreDataEntry
+    {
+        public string user;
+        public int score;
+
+        public HighscoreDataEntry()
+        {
+            this.user = "";
+            this.score = 0;
+        }
+    }
+
+    public List<HighscoreDataEntry> entries;
+
+    public HighscoreData()
+    {
+        entries = new List<HighscoreDataEntry>();
+    }
+}
---
+++ Assets/Scripts/NetworkScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkScript : MonoBehaviour {
+
+    public static System.Action<HighscoreData> OnHighscoreDataReceived;
+
+    public string uri = "http://www.google.com";
+
+    [ContextMenu("Test")]
+	void Start () {
+        StartCoroutine(GetText());
+	}
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(uri);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+        } else
+        {
+            // successful
+            Debug.Log(www.downloadHandler.text);
+
+            // parse json text
+            HighscoreData data = JsonUtility.FromJson<HighscoreData>(www.downloadHandler.text);
+            
+            //Debug.Log(data.entries);
+            //for (int i = 0; i < data.entries.Count; i++)
+            //{
+            //    Debug.Log(data.entries[i].user + ": " + data.entries[i].score);
+            //}
+
+        if (OnHighscoreDataReceived != null)
+            {
+                OnHighscoreDataReceived(data);
+            }
+        }
+    }
+}
